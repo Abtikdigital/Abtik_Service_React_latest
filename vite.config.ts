c1f69d4e9495a311373354e6b1eb24a9c3d5a3e5
Abtikdigital <rbhensla1994@gmail.com>
2025-08-26 16:55:16 +0530

    updated header files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(),react()],
-})
+  plugins: [tailwindcss(), react()],
+  build: {
+    sourcemap: false, // ❌ no source maps at all, safer for production
+  },
+});
