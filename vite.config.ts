d97c50e9461884725e18ae319c98ab3a2391c9f1
Abtikdigital <rbhensla1994@gmail.com>
2025-08-26 17:01:02 +0530

    updated vite config js

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  build: {
-    sourcemap: false, // ❌ no source maps at all, safer for production
-  },
-});
+  plugins: [ tailwindcss(),react()],
+  
+})
