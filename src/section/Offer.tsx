c2b8e0d0b6c7dce2cbd6cfb8f66d241c638afe31
Abtikdigital <rbhensla1994@gmail.com>
2025-08-27 11:37:29 +0530

    updated Annoucement bar

--- src/section/Offer.tsx
+++ src/section/Offer.tsx
@@ -1,27 +1,27 @@
 // import Icon from "../assets/icons/glass.png";
 
 const WebsiteOfferBanner = () => {
-  // const message = "Our 3rd Year of Togetherness, Forever to Go";
+  const message = "🌸 मंगलमूर्ति मोरया | Celebrate with love, happiness & blessings 🎉";
 
   return (<>
 
-     {/* <div className="relative bg-gradient-to-r px-3 from-[#3CA2E2] to-[#052EAA] text-white py-3 overflow-hidden shadow-[0_0_20px_rgba(60,162,226,0.6)]">
+     <div className="relative bg-gradient-to-r px-3 from-[#3CA2E2] to-[#052EAA] text-white py-3 overflow-hidden shadow-[0_0_20px_rgba(60,162,226,0.6)]">
       
        <div className="flex w-max animate-marquee gap-12">
          {[...Array(6)].map((_, i) => (
            <div key={i} className="flex items-center gap-3 whitespace-nowrap">
-             <span className="flex-shrink-0 w-6 h-6">
+             {/* <span className="flex-shrink-0 w-6 h-6">
                <img src={Icon} alt="icon" className="w-full h-full" />
-             </span>
+             </span> */}
              <span
                className="relative inline-block text-lg font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-white via-[#3CA2E2] to-white"
                style={{ fontFamily: "Playfair Display" }}
              >
                {message}
              </span>
-             <span className="flex-shrink-0 w-6 h-6">
+             {/* <span className="flex-shrink-0 w-6 h-6">
                <img src={Icon} alt="icon" className="w-full h-full" />
-             </span>
+             </span> */}
            </div>
          ))}
        </div>
@@ -39,7 +39,7 @@ const WebsiteOfferBanner = () => {
            }
          `}
        </style>
-     </div>  */}
+     </div> 
     </>
   );
 };
