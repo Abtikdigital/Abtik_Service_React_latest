7fbfdd9068312bba48f29c564a5720b007cdba93
Abtik Digital <rbhensla1994@gmail.com>
2025-09-09 11:03:58 +0530

    updated offer

--- src/section/Offer.tsx
+++ src/section/Offer.tsx
@@ -1,18 +1,16 @@
 // import Icon from "../assets/icons/glass.png";
 
 const WebsiteOfferBanner = () => {
-  const message = " मंगलमूर्ति मोरया | Celebrate with love, happiness & blessings ";
+  // const message = " मंगलमूर्ति मोरया | Celebrate with love, happiness & blessings ";
 
   return (<>
 
-     <div className="relative bg-gradient-to-r px-3 from-[#3CA2E2] to-[#052EAA] text-white py-3 overflow-hidden shadow-[0_0_20px_rgba(60,162,226,0.6)]">
+     {/* <div className="relative bg-gradient-to-r px-3 from-[#3CA2E2] to-[#052EAA] text-white py-3 overflow-hidden shadow-[0_0_20px_rgba(60,162,226,0.6)]">
       
        <div className="flex w-max animate-marquee gap-12">
          {[...Array(6)].map((_, i) => (
            <div key={i} className="flex items-center gap-3 whitespace-nowrap">
-             {/* <span className="flex-shrink-0 w-6 h-6">
-               <img src={Icon} alt="icon" className="w-full h-full" />
-             </span> */}
+          
              🌸
              <span
                className="relative inline-block text-lg font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-white via-[#3CA2E2] to-white"
@@ -21,9 +19,7 @@ const WebsiteOfferBanner = () => {
                {message}
              </span>
              🎉
-             {/* <span className="flex-shrink-0 w-6 h-6">
-               <img src={Icon} alt="icon" className="w-full h-full" />
-             </span> */}
+           
            </div>
          ))}
        </div>
@@ -41,7 +37,7 @@ const WebsiteOfferBanner = () => {
            }
          `}
        </style>
-     </div> 
+     </div>  */}
     </>
   );
 };
